e2de8306111d550dbd3a9c946395d3ab7b65c4e8
Dev <devrai197551@gmail.com>
2026-02-13 03:31:55 +0530

    Add TodosController with full CRUD API also added CORS

--- Server/Program.cs
+++ Server/Program.cs
@@ -7,12 +7,23 @@ Env.Load();
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = Environment.GetEnvironmentVariable("DATABASE_CONNECTION_STRING");
 
-builder.Services.AddDbContext<MyTodoAppDbContext>(options =>
-    options.UseNpgsql(connectionString));
-  builder.Services.AddControllers();
+builder.Services.AddDbContext
+<MyTodoAppDbContext>(options => options.UseNpgsql(connectionString));
+builder.Services.AddControllers();
+
+builder.Services.AddCors(options =>
+{
+  options.AddPolicy("AllowReactApp", policy =>
+    {
+      policy.WithOrigins("http://localhost:5173", "http://localhost:3000")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
+app.UseCors("AllowReactApp");
 app.UseHttpsRedirection();
 app.MapControllers();
 
